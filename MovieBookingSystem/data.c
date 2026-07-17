6b711884effba4b7e95015d1c59af974c1441903
Thanuka <thanukathathsara20@gmail.com>
2026-07-17 11:53:09 +0530

    Added movie,showtime list and seat prices

--- MovieBookingSystem/data.c
+++ MovieBookingSystem/data.c
@@ -4,3 +4,14 @@
 //Hard coded movie list
 
 
+// Avengers: Endgame | Show time :10.00 am to 1.00 pm and 3.00 pm to 6.00 pm
+// Oppenheimer       | Show time :10.00 am to 1.00 pm and 3.00 pm to 6.00 pm
+// Interstellar      | Show time :10.00 am to 1.00 pm and 3.00 pm to 6.00 pm
+// Deadpool & Wolverine | Show time :10.00 am to 1.00 pm and 3.00 pm to 6.00 pm
+// The Batman        | Show time :10.00 am to 1.00 pm and 3.00 pm to 6.00 pm
+
+//Seat pricing list
+
+//Regular (Rows 1-2) Rs. 500
+//Premium (Rows 3-4) Rs. 750
+//VIP (Row 5)        Rs. 1000
