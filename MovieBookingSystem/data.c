ecb7bba0417d9647cc87962443fe66ccfd8f8251
Omira <omiragk@outlook.com>
2026-07-17 13:13:12 +0530

    Created structs for Seat and MovieShow

--- MovieBookingSystem/data.c
+++ MovieBookingSystem/data.c
@@ -15,3 +15,28 @@
 //Regular (Rows 1-2) Rs. 500
 //Premium (Rows 3-4) Rs. 750
 //VIP (Row 5)        Rs. 1000
+
+
+#define MOVIES 5
+#define SHOWTIMES 2
+#define ROWS 6
+#define COLS 10
+
+typedef struct {
+    int booked;
+    char customerName[50];
+    float pricePaid;
+} Seat;
+
+typedef struct
+{
+    char movieTitle[50];
+    char showTime[20];
+
+    Seat seats[ROWS][COLS];
+
+    float revenue;
+    int ticketsSold;
+
+} MovieShow;
+
