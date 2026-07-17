3e99c4908b73c83f78096655aed918e82dbf68bf
Omira <omiragk@outlook.com>
2026-07-17 22:56:23 +0530

    Added Discount Calculating function

--- MovieBookingSystem/pricing.c
+++ MovieBookingSystem/pricing.c
@@ -5,3 +5,26 @@
 // Group discount (4 or more than 4 ) - 10%
 
 //only one discount should be applicable except the group discount
+
+
+float calculateTicketPrice(float basePrice, int totalTickets, int isStudent, int isSenior)
+{
+    float discountPercentage = 0.0;
+
+    if(isSenior == 1)
+    {
+        discountPercentage+=0.20;
+    }
+    else if(isStudent == 1)
+    {
+        discountPercentage+=0.10;
+    }
+
+    if(totalTickets >= 4)
+    {
+        discountPercentage += 0.10;
+    }
+
+    float finalPrice = (basePrice*1.0)-(basePrice*discountPercentage);
+    return finalPrice;
+}
