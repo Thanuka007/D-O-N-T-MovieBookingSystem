0973e9783774d73ec9e858d3d7878cd6d8080345
senathx <dsenath.web@gmail.com>
2026-07-17 13:21:12 +0530

    Created the option menu of the booking system

--- MovieBookingSystem/display.c
+++ MovieBookingSystem/display.c
@@ -1,3 +1,30 @@
+#include <stdio.h>
+
+
+void DisplayMenu(){
+
+    printf("-----------------------------------\n");
+    printf("        Lights Camera Book!        \n");
+    printf("-----------------------------------\n");
+
+    printf("Welcome to the Movie Booking system!\n");
+    printf("-----------------------------------\n");
+
+    printf("    1. View Showtimes\n");
+    printf("    2. View Seat Map\n");
+    printf("    3. Book a Seat\n");
+    printf("    4. Cancel a Booking\n");
+    printf("    5. Search Booking\n");
+    printf("    6. View Revenue Report\n");
+    printf("    7. Exit\n");
+
+    printf("-----------------------------------\n\n");
+
+}
+
+
+
+
  // Show the list of all movies
 
  //Print options to book,choose seat, cancel a boooking
