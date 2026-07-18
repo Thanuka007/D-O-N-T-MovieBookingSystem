45f118dcd46f92f7f2181a4b018ce08d100ce770
Omira <omiragk@outlook.com>
2026-07-18 09:51:14 +0530

    added backend function to remove data from struct

--- MovieBookingSystem/booking.c
+++ MovieBookingSystem/booking.c
@@ -47,3 +47,15 @@ void addDataToArray(int movieIndex,int showtimeIndex,int row,int col, char userN
     movies[movieIndex][showtimeIndex].ticketsSold++;
     movies[movieIndex][showtimeIndex].revenue += finalPrice;
 }
+
+void removeDataFromArray(int movieIndex,int showtimeIndex,int row,int col,float paidPrice)
+{
+    //update seat data
+    movies[movieIndex][showtimeIndex].seats[row][col].booked = 0;
+    strcpy(movies[movieIndex][showtimeIndex].seats[row][col].customerName," ");
+    movies[movieIndex][showtimeIndex].seats[row][col].pricePaid = 0.0;
+
+    // Update movie show totals
+    movies[movieIndex][showtimeIndex].ticketsSold--;
+    movies[movieIndex][showtimeIndex].revenue -= paidPrice;
+}
